5724ee05d305499d6a594a13533af369db94e1a8
Administrator <Administrator@DESKTOP-BEJOVSO>
2024-08-22 19:36:56 +0300

    Add project files.

---
+++ List1/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Bir liste oluşturuyoruz ve ziyaretçilerin isimlerini ekliyoruz
+List<string> davetliler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+//  Liste içerisinde for döngüsü ile dönerek isimlerini ekrana yazdırma
+Console.WriteLine("* Davetliler *");
+int index = 1;
+foreach (string davetli in davetliler)
+{
+    Console.WriteLine("{0} - {1}", index++, davetli);
+}
